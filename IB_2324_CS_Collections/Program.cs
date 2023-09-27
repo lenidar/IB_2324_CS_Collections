5ca1ed7b5610be75f19d310f120265bdd26e6403
lenidar <55443074+lenidar@users.noreply.github.com>
2023-09-27 09:08:10 +0800

    Add project files.

---
+++ IB_2324_CS_Collections/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IB_2324_CS_Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int[] numbers = new int[rnd.Next(5, 10000)];
+
+            numbers[0] = rnd.Next(5, 10000); // assignment of a value to an index
+
+            for(int x = 1; x < numbers.Length; x++)
+            {
+                numbers[x] = numbers[x - 1] + 1; // assignment of a value based on another index
+            }
+
+            for(int x = 0; x < numbers.Length; x++)
+            {
+                Console.WriteLine(numbers[x]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
